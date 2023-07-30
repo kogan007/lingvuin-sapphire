590270e54dbe947adaeaa4478a3f08770c7588d3
Corey Kogan <coreykogan@gmail.com>
2023-07-30 01:39:15 -0400

    Update MessageCreate.ts

--- src/listeners/MessageCreate.ts
+++ src/listeners/MessageCreate.ts
@@ -1,6 +1,8 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Listener } from '@sapphire/framework';
-import type { Message } from 'discord.js';
+import { Collection, type Message } from 'discord.js';
+
+const xpCooldown = new Collection<string, number>();
 
 @ApplyOptions<Listener.Options>({})
 export class UserEvent extends Listener {
@@ -14,10 +16,41 @@ export class UserEvent extends Listener {
 	public override async run(message: Message) {
 		const user = await this.container.utils.getUserById(message.author.id);
 		if (user) {
+			const { levelUp, xpToAdd, cooldown } = calcUserXp(message, user.experience);
 			await this.container.utils.updateUserById(message.author.id, {
 				money: user.money + 1,
-				messagesSent: user.messagesSent + 1
+				messagesSent: user.messagesSent + 1,
+				experience: cooldown ? user.experience : user.experience + xpToAdd
 			});
+
+			if (levelUp) {
+				console.log(`${message.author} has leveled up!`);
+			}
 		}
 	}
 }
+
+function calcUserXp(message: Message, currentXp: number) {
+	const userCooldown = xpCooldown.get(message.author.id);
+	const now = Date.now();
+	let cooldown = false;
+	if (userCooldown) {
+		if (now - userCooldown < 1000 * 10) cooldown = true;
+		else xpCooldown.set(message.author.id, now);
+	} else {
+		xpCooldown.set(message.author.id, now);
+	}
+
+	const currentLevel = Math.floor(0.15 * Math.sqrt(currentXp + 1));
+	const xpToAdd = randomInt(5, 25);
+	const nextLevel = Math.floor(0.15 * Math.sqrt(currentXp + xpToAdd + 1));
+	const levelUp = nextLevel > currentLevel;
+
+	return {
+		levelUp,
+		xpToAdd,
+		cooldown
+	};
+}
+
+const randomInt = (min: number, max: number) => min + Math.floor((max - min) * Math.random());
