ac9635d6d8595af60243b2ca423f9fd2eb2239af
Corey Kogan <coreykogan@gmail.com>
2023-07-30 05:14:24 -0400

    Update Profile.ts

--- src/commands/User/Profile.ts
+++ src/commands/User/Profile.ts
@@ -1,5 +1,5 @@
 import { Command } from '@sapphire/framework';
-import { EmbedBuilder } from 'discord.js';
+// import { EmbedBuilder } from 'discord.js';
 import canvacord from 'canvacord';
 
 export class ProfileCommand extends Command {
@@ -22,14 +22,16 @@ export class ProfileCommand extends Command {
 		await interaction.deferReply();
 		const userToView = interaction.options.getUser('user') ?? interaction.user;
 		const user = await this.container.utils.getUserById(userToView.id);
-		const reputation = user.reputation || [];
+		// const reputation = user.reputation || [];
 
 		const level = Math.floor(0.15 * Math.sqrt(user.experience + 1));
+		console.log(Math.floor((level / 0.15) * (level / 0.15)));
 		const rank = new canvacord.Rank()
 			.setAvatar(userToView.avatarURL() ?? '')
 			.setCurrentXP(user.experience ?? 0)
 			.setRequiredXP(Math.floor((level / 0.15) * (level / 0.15)))
 			.setProgressBar('#FFFFFF', 'COLOR')
+			.setLevel(level)
 			.setUsername(userToView.username);
 
 		const image = await rank.build();
